6daeca0509ceae2d8426af7702835a8002a38e69
Jeandre <jeandrev1414@gmail.com>
2022-07-07 11:08:15 -0600

    Fixed spelling errors

--- survey.js
+++ survey.js
@@ -12,7 +12,7 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
         rl.question('What\'s your favourite thing to eat for that meal? ', (favoriteMeal) => {
           rl.question('Which sport is your absolute favourite? ' , (sport) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (power) => {
-              console.log(`${name} enjoys ${activity} while listening to ${music}. For ${mealeal}, ${name} likes to have ${favoriteMeal}. ${name} loves to play ${sport}, and is very good at ${power}!`);
+              console.log(`${name} enjoys ${activity} while listening to ${music}. For ${meal}, ${name} likes to have ${favoriteMeal}. ${name} loves to play ${sport}, and is very good at ${power}!`);
               rl.close();
             })
           })
